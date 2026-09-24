c1e1e90281f523495bfec68d49f10b8e41803e54
Deaw23456 <somjaiyonlawat@gmail.com>
2026-09-25 01:40:43 +0700

    add midreware

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ def create_app() -> FastAPI:
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=ALLOWED_ORIGINS,
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
